bea89a57d64862bada8debd3ad30d206317b3d8a
Nathan Luke Abraham <luke@atm-ukca3.local>
2022-02-09 17:18:51 +0000

    install ec2-instance-connect

--- install-aws.sh
+++ install-aws.sh
@@ -1,9 +1,6 @@
-#### Install the X2Go for desktop connections - only for Ubuntu currently
+#### Install ec2-instance-connect for AWS EC2 instances, only needed if using Ubuntu 18.04
 if [[ $dist == ubuntu ]]; then
   if [[ $release == 1804 ]]; then
-    apt-get install -q -y software-properties-common || error
-    add-apt-repository -y ppa:x2go/stable || error
-    apt-get -q -y update || error
-    apt-get install -q -y x2goserver x2goserver-xsession x2golxdebindings || error
+    apt-get install -q -y ec2-instance-connect || error
   fi
 fi
